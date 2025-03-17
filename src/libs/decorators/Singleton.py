29e37c7b864b7fae1a0d5e52a4de5eaad00f739e
vbetsch <vic.betsch@gmail.com>
2025-03-17 16:33:14 +0100

    :bug: Improve Singleton

--- src/libs/decorators/Singleton.py
+++ src/libs/decorators/Singleton.py
@@ -1,9 +1,13 @@
 def Singleton(cls):
-    instances = {}
+    cls._instance = None
 
     def get_instance(*args, **kwargs):
-        if cls not in instances:
-            instances[cls] = cls(*args, **kwargs)
-        return instances[cls]
+        if cls._instance is None:
+            cls._instance = cls(*args, **kwargs)
+        return cls._instance
 
+    def clear_instance():
+        cls._instance = None
+
+    get_instance.clear_instance = clear_instance
     return get_instance

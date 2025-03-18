a9d959539590e0c05d5e32a8476d59b30dbe7655
vbetsch <vic.betsch@gmail.com>
2025-03-18 15:07:42 +0100

    :truck: Entities - Move score into session
    Signed-off-by: vbetsch <vic.betsch@gmail.com>

--- src/domain/entities/Player.py
+++ src/domain/entities/Player.py
@@ -1,7 +1,4 @@
-from src.domain.entities.Score import Score
-
-
 class Player:
     def __init__(self, name: str) -> None:
         self._name = name
-        self._score: Score = Score()
+        # TODO: sessions
--- src/domain/entities/Session.py
+++ src/domain/entities/Session.py
@@ -1,6 +1,8 @@
+from src.domain.entities.Score import Score
 from src.domain.values.StateEnum import StateEnum
 
 
 class Session:
     def __init__(self, state: StateEnum) -> None:
         self._state: StateEnum = state
+        self._score: Score = Score()

f02a51c2ff9321e4622d97aa61c08ec4fbe955d9
vbetsch <vic.betsch@gmail.com>
2025-03-18 15:15:27 +0100

    :sparkles: Implements Memento pattern
    Signed-off-by: vbetsch <vic.betsch@gmail.com>

--- src/domain/entities/Session.py
+++ src/domain/entities/Session.py
@@ -1,8 +1,17 @@
 from src.domain.entities.Score import Score
 from src.domain.values.StatusEnum import StatusEnum
+from src.domain.values.sessions.SessionMemento import SessionMemento
 
 
 class Session:
     def __init__(self, state: StatusEnum) -> None:
         self._state: StatusEnum = state
         self._score: Score = Score()
+
+    def save(self) -> SessionMemento:
+        return SessionMemento(self)
+
+    def restore(self, memento: SessionMemento) -> None:
+        session = memento.get_saved_state()
+        self._state = session._state
+        self._score = session._score
--- src/domain/values/sessions/SessionHistory.py
+++ src/domain/values/sessions/SessionHistory.py
@@ -1,2 +1,19 @@
+from src.domain.entities.Session import Session
+from src.domain.values.sessions.SessionMemento import SessionMemento
+
+
 class SessionHistory:
-    pass
+    def __init__(self) -> None:
+        self._mementos: list[SessionMemento] = []
+
+    def save(self, session: Session) -> None:
+        memento = session.save()
+        self._mementos.append(memento)
+
+    def undo(self, session: Session) -> None:
+        if len(self._mementos) <= 1:
+            raise Exception("No sessions to restore")
+
+        self._mementos.pop()
+        last_memento: SessionMemento = self._mementos[-1]
+        session.restore(last_memento)

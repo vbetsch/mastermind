6b07d8947041732afdd067e78192f8a36dd82c70
vbetsch <vic.betsch@gmail.com>
2025-03-24 00:21:58 +0100

    :truck: Transform Turn & Combination to value objects

--- src/domain/values/combinations/Combination.py
+++ src/domain/values/combinations/Combination.py
@@ -1,10 +1,9 @@
-from dataclasses import dataclass, field
-
 from src.domain.values.combinations.Bead import Bead
 
-@dataclass(frozen=True)
+
 class Combination:
-    beads: list[Bead] = field(default_factory=list[Bead])
+    def __init__(self, beads: list[Bead]):
+        self.beads: list[Bead] = beads
 
     def __repr__(self):
         return f"Combination={[bead.color.value for bead in self.beads]}"
--- src/domain/values/sessions/Turn.py
+++ src/domain/values/sessions/Turn.py
@@ -1,12 +1,10 @@
-from dataclasses import dataclass
-
 from src.domain.values.StatusEnum import StatusEnum
 from src.domain.values.combinations.Combination import Combination
 from src.domain.values.turns.Feedback import Feedback
 
 
-@dataclass(frozen=True)
 class Turn:
-    status: StatusEnum
-    feedback: Feedback
-    proposal: Combination | None = None
+    def __init__(self):
+        self.status: StatusEnum = StatusEnum.NOT_STARTED
+        self.feedback: Feedback | None = None
+        self.proposal: Combination | None = None

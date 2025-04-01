fdffcf526478f5ecf0a9eea867aa052e4e77c127
vbetsch <vic.betsch@gmail.com>
2025-04-02 00:48:52 +0200

    :sparkles: Implement Indicator.value

--- src/domain/values/turns/indicators/Indicator.py
+++ src/domain/values/turns/indicators/Indicator.py
@@ -4,3 +4,10 @@ from src.domain.values.turns.indicators.IndicatorColorEnum import IndicatorColor
 class Indicator:
     def __init__(self, color: IndicatorColorEnum) -> None:
         self._color: IndicatorColorEnum = color
+        self._value: int = 0
+
+    def get_value(self) -> int:
+        return self._value
+
+    def set_value(self, value: int) -> None:
+        self._value = value

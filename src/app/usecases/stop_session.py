38cfdb190661c450a165ac5bb663c2497823bc93
vbetsch <vic.betsch@gmail.com>
2025-04-02 22:35:54 +0200

    :label: Fix typing

--- src/app/usecases/stop_session.py
+++ src/app/usecases/stop_session.py
@@ -1,10 +1,11 @@
 from src.app.ports.usecases.IStopSessionUseCase import IStopSessionUseCase
 from src.domain.core.Game import Game
+from src.domain.entities.Session import Session
 
 
 class StopSession(IStopSessionUseCase):
     def execute(self, arg=None) -> None:
-        session = Game().get_current_session()
+        session: Session | None = Game().get_current_session()
         if session is not None:
             session.stop()
             Game().set_current_session(session)

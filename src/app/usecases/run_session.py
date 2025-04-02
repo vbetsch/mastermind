01b09891cda73038e762ac2637f7a4cd22e94c16
vbetsch <vic.betsch@gmail.com>
2025-04-02 22:56:06 +0200

    :sparkles: Add save_when_played

--- src/app/usecases/run_session.py
+++ src/app/usecases/run_session.py
@@ -6,4 +6,8 @@ class RunSession(IRunSessionUseCase):
     def execute(self, arg=None) -> None:
         session = Game().get_current_session()
         session.run()
+
+        if Game().get_save_when_played() is True:
+            session.save()
+
         Game().set_current_session(session)
--- src/domain/core/Game.py
+++ src/domain/core/Game.py
@@ -7,22 +7,26 @@ from src.domain.values.players.StateEnum import StateEnum
 @Singleton
 class Game:
     def __init__(self) -> None:
-        self.currentPlayer: Player = Player(name="default", state=StateEnum.INSIDE_MENUS)
-        self.currentSession: Session | None = None
-        self.beads_per_combination: int = 4
-        self.max_attempts_before_loose: int = 12
+        self._currentPlayer: Player = Player(name="default", state=StateEnum.INSIDE_MENUS)
+        self._currentSession: Session | None = None
+        self._save_when_played: bool = False
+        self._beads_per_combination: int = 4
+        self._max_attempts_before_loose: int = 12
 
     def get_current_player(self) -> Player:
-        return self.currentPlayer
+        return self._currentPlayer
 
     def get_current_session(self) -> Session | None:
-        return self.currentSession
+        return self._currentSession
+
+    def get_save_when_played(self) -> bool:
+        return self._save_when_played
 
     def get_beads_per_combination(self) -> int:
-        return self.beads_per_combination
+        return self._beads_per_combination
 
     def get_max_attempts_before_loose(self) -> int:
-        return self.max_attempts_before_loose
+        return self._max_attempts_before_loose
 
     def set_current_session(self, session: Session) -> None:
-        self.currentSession = session
+        self._currentSession = session

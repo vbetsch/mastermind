15a6bfe366b41d5c085f86d8d3461e4c27f5ac06
vbetsch <vic.betsch@gmail.com>
2025-04-02 23:17:07 +0200

    :label: Review Player typing

--- src/common/communication/Data.py
+++ src/common/communication/Data.py
@@ -2,5 +2,5 @@ from src.domain.entities.Player import Player
 
 
 class Data:
-    def __init__(self, player: Player | None = None) -> None:
+    def __init__(self, player: Player = None) -> None:
         self.player: Player | None = player

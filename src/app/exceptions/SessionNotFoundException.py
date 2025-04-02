9de77e232789f2eecd1a7410480c53603cdffb37
vbetsch <vic.betsch@gmail.com>
2025-04-03 00:11:51 +0200

    :bug: Fix possible bug Create SessionNotFoundException

---
+++ src/app/exceptions/SessionNotFoundException.py
@@ -0,0 +1,3 @@
+class SessionNotFoundException(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
--- src/app/usecases/run_session.py
+++ src/app/usecases/run_session.py
@@ -1,10 +1,14 @@
+from src.app.exceptions.SessionNotFoundException import SessionNotFoundException
 from src.app.ports.usecases.IRunSessionUseCase import IRunSessionUseCase
 from src.domain.core.Rules import Rules
 from src.domain.core.Storage import Storage
+from src.domain.entities.Session import Session
 
 
 class RunSession(IRunSessionUseCase):
     def execute(self) -> None:
-        session = Storage().get_current_session()
+        session: Session | None = Storage().get_current_session()
+        if session is None:
+            raise SessionNotFoundException('No session found')
         session.run()
         Storage().set_current_session(session)

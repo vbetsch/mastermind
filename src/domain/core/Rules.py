98602c131be9a21ea4433c451ae5f744dd620eab
vbetsch <vic.betsch@gmail.com>
2025-04-04 01:49:55 +0200

    :hammer: Enable dev mode

--- src/domain/core/Rules.py
+++ src/domain/core/Rules.py
@@ -4,7 +4,7 @@ from src.common.decorators.Singleton import Singleton
 @Singleton
 class Rules:
     def __init__(self) -> None:
-        self._dev_mode: bool = False
+        self._dev_mode: bool = True
         self._save_when_played: bool = False
         self._beads_per_combination: int = 4
         self._max_attempts_before_loose: int = 12

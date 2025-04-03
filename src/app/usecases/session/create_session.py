1c5e05b5823daa6d91f7e1fa9545ecccbc2e433e
vbetsch <vic.betsch@gmail.com>
2025-04-04 01:57:21 +0200

    :bug: Move the reveal into GetPreviousProposals

--- src/app/usecases/session/create_session.py
+++ src/app/usecases/session/create_session.py
@@ -1,7 +1,5 @@
 from src.app.ports.usecases.session.ICreateSessionUseCase import ICreateSessionUseCase
-from src.common.logs.Logger import Logger
 from src.domain.core.Generator import Generator
-from src.domain.core.Rules import Rules
 from src.domain.core.Storage import Storage
 from src.domain.entities.Player import Player
 from src.domain.entities.Session import Session
@@ -10,9 +8,5 @@ from src.domain.entities.Session import Session
 class CreateSession(ICreateSessionUseCase):
     def execute(self, player: Player) -> None:
         new_session: Session = Session(player, Generator().generate_combination())
-
-        if Rules().get_dev_mode() is True:
-            Logger().info(f"The response is {new_session.secret_combination}")
-
         player.sessions.save(new_session)
         Storage().set_current_session(new_session)

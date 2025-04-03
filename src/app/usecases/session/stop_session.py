58a024dca937ae868c5eff7b72db46251870a078
vbetsch <vic.betsch@gmail.com>
2025-04-04 01:09:43 +0200

    :bug: Stop Session only if not already done

--- src/app/usecases/session/stop_session.py
+++ src/app/usecases/session/stop_session.py
@@ -1,11 +1,12 @@
 from src.app.ports.usecases.session.IStopSessionUseCase import IStopSessionUseCase
 from src.domain.core.Storage import Storage
 from src.domain.entities.Session import Session
+from src.domain.values.StatusEnum import StatusEnum
 
 
 class StopSession(IStopSessionUseCase):
     def execute(self) -> None:
         session: Session | None = Storage().get_if_session()
-        if session is not None:
+        if session is not None and session.status != StatusEnum.STOPPED:
             session.stop()
             Storage().set_current_session(session)

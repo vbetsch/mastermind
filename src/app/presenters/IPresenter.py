1542426fd387a2f4478d2c87c9c622758b84ddd5
vbetsch <vic.betsch@gmail.com>
2025-04-05 00:02:41 +0200

    :sparkles: Presenters (v2)

--- src/app/presenters/IPresenter.py
+++ src/app/presenters/IPresenter.py
@@ -1,5 +1,13 @@
+from abc import ABC, abstractmethod
+
+from src.app.ports.data.IData import IData
 from src.common.communication.dto.IDto import IDto
 
 
-class IPresenter(IDto):
-    pass
+class IPresenter(ABC):
+    def __init__(self, data: IData):
+        self.data: IData = data
+
+    @abstractmethod
+    def present(self) -> IDto:
+        pass

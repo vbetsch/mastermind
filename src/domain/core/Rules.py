0071e85325c1442c7e6343573f6e3a677fe6e692
vbetsch <vic.betsch@gmail.com>
2025-04-05 20:38:41 +0200

    :sparkles: Set Rules to default values

--- src/domain/core/Rules.py
+++ src/domain/core/Rules.py
@@ -4,7 +4,7 @@ from src.common.decorators.Singleton import Singleton
 @Singleton
 class Rules:
     def __init__(self) -> None:
-        self._dev_mode: bool = True  # Display logs and secret combination (default: False)
+        self._dev_mode: bool = False  # Display logs and secret combination (default: False)
         self._save_when_played: bool = False  # Auto-save (default: False)
         self._beads_per_combination: int = 4  # Combinations length (default: 4)
         self._max_attempts_before_loose: int = 12  # Board size (default: 12)

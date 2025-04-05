262ac29f565e4018c4e3353cd48561e662a08777
vbetsch <vic.betsch@gmail.com>
2025-04-05 17:41:00 +0200

    :loud_sound: Add logs

--- src/app/usecases/state/get_state.py
+++ src/app/usecases/state/get_state.py
@@ -1,4 +1,5 @@
 from src.app.ports.usecases.state.IGetState import IGetState
+from src.common.logs.Logger import Logger
 from src.domain.core.Arbitrator import Arbitrator
 from src.domain.core.Storage import Storage
 from src.domain.entities.Session import Session
@@ -10,10 +11,13 @@ class GetState(IGetState):
     def execute(self) -> StateEnum:
         session: Session = Storage().get_current_session()
         if Arbitrator().has_reached_max_attempts(session):
+            Logger().error(f"LOST : Reached max attempts")
             return StateEnum.LOST
 
         turn: Turn = Storage().get_current_turn()
         if Arbitrator().has_valid_combination(turn.get_feedback()):
+            Logger().ok(f"WIN : good answer !")
             return StateEnum.WON
 
+        Logger().debug(f"Try again")
         return StateEnum.PLAYING
